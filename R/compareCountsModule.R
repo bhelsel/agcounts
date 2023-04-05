170114d320a089843126b33b69f9e819c4b0c003
Brian Helsel <bhelsel80@gmail.com>
2023-04-05 15:50:25 -0500

    Develop a Shiny app for agcounts
    Developed a Shiny app to allow users to visualize, explore, and compare activity counts.

---
+++ R/compareCountsModule.R
@@ -0,0 +1,129 @@
+#' @title compareCountsModuleUI
+#' @description UI for compareCountsModule
+#' @noRd
+#' @keywords internal
+#' @importFrom reactable reactableOutput
+#'
+
+compareCountsModuleUI <- function(id){
+  ns <- shiny::NS(id)
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(
+      shiny::fileInput(ns("agdFile"), "Choose the Matching AGD File", multiple = FALSE, accept = c(".agd")),
+      shiny::selectInput(ns("axisCounts2"), "Count Axis", choices = c("Axis1", "Axis2", "Axis3", "Vector.Magnitude"), selected = "Vector.Magnitude"),
+      shiny::checkboxInput(ns("agdBlandAltmanPlot"), "Bland Altman Plot?", value = FALSE),
+      shiny::uiOutput(ns("rangeYBlandAltman")),
+      shiny::textInput(ns("agdPlotColor"), "Plot Color (accepts color name or hex code)", value = "#000000"),
+    ),
+    shiny::mainPanel(
+      shiny::tabsetPanel(
+        id = "comparisonTabset",
+        shiny::tabPanel("Visualization",
+                        shiny::plotOutput(ns("comparisonPlot"), width = "100%")),
+        shiny::tabPanel("Data",
+                        shiny::HTML("<h5>Differences between ActiGraph counts and agcounts</h5>"),
+                        reactable::reactableOutput(ns("comparisonReactableTable")))
+      )
+    )
+  )
+}
+
+#' @title compareCountsModuleServer
+#' @description Server for compareCountsModuleServer
+#' @noRd
+#' @keywords internal
+#' @importFrom dplyr mutate rename select
+#' @importFrom stringr regex
+#' @importFrom reactable renderReactable reactable colDef
+#' @importFrom grDevices colors
+#' @importFrom stats sd
+
+compareCountsModuleServer <- function(id, filteredData){
+  shiny::moduleServer(id, function(input, output, session){
+
+    # Dynamic UI ----
+    # Dynamic UI components generated from the server
+    output$rangeYBlandAltman <- shiny::renderUI({
+      shiny::req(comparisonData(), input$agdBlandAltmanPlot)
+      ad <- max(abs(comparisonData()$Difference))
+      mn <- mean(comparisonData()$Difference) - (1.96 * stats::sd(comparisonData()$Difference))
+      mx <- mean(comparisonData()$Difference) + (1.96 * stats::sd(comparisonData()$Difference))
+      shiny::sliderInput(session$ns("rangeYBlandAltman"),
+                         "Adjust the Y-Axis of the Bland Altman Plot",
+                         min = -ad, max = ad, value = c(mn, mx),
+                         post = " counts", ticks = FALSE)
+    })
+
+    # Data Processing ----
+
+    agdDataComparison <- shiny::reactive({
+      shiny::req(input$agdFile)
+      date2filter <- as.Date(filteredData()[1, "time"])
+      agdData <- .read_agd(input$agdFile$datapath)
+      filter <- ifelse(agdData[["filter"]] == "LowFrequencyExtension", TRUE, FALSE)
+      epoch_length <- agdData[["epoch_length"]]
+      data <- agdData[["data"]][as.Date(agdData[["data"]]$time) == date2filter, ]
+      return(list(filter = filter, epoch_length = epoch_length, data = data))
+    })
+
+    comparisonData <- shiny::reactive({
+      shiny::req(filteredData(), agdDataComparison())
+      Average <- Difference <- NULL
+      agd <- agdDataComparison()
+      raw <- filteredData()[, 1:4]
+      class(raw) <- "data.frame"
+      agcounts <- calculate_counts(raw, epoch = agd[["epoch_length"]], lfe_select = agd[["filter"]], tz = "UTC")
+      agcounts %<>% dplyr::select(time, input$axisCounts2) %>% `colnames<-`(c("time", "agcounts"))
+      agd[["data"]] %<>% dplyr::select(time, input$axisCounts2) %>% `colnames<-`(c("time", "agd"))
+      df <- merge(agcounts, agd[["data"]], by = "time")
+      df$Difference <- df$agcounts - df$agd
+      df$Average <- (df$agcounts + df$agd) / 2
+      return(df)
+    })
+
+    # Visualization ----
+    output$comparisonPlot <- shiny::renderPlot({
+      hexFormat <- stringr::regex("^#([A-Fa-f0-9]{6})$")
+      agdColor <- ifelse(input$agdPlotColor %in% grDevices::colors() | grepl(hexFormat, input$agdPlotColor), input$agdPlotColor, "#000000")
+      df <- comparisonData()
+      p <- ggplot2::ggplot(data = df) +
+        ggplot2::geom_point(ggplot2::aes(x = agd, y = agcounts), color = agdColor) +
+        ggplot2::labs(title = "ActiGraph counts vs. agcounts", x = "ActiGraph Counts from the AGD FIle", y = "agcounts") +
+        ggplot2::theme_minimal() +
+        .agPlotTheme()
+
+      if(input$agdBlandAltmanPlot){
+        p <- ggplot2::ggplot(data = df, ggplot2::aes(x = Average, y = Difference)) +
+          ggplot2::geom_point(size = 1, color = agdColor) +
+          ggplot2::geom_hline(yintercept = mean(df$Difference), color = "#000000", linewidth = 0.5) +
+          ggplot2::geom_hline(yintercept = mean(df$Difference) - (1.96 * stats::sd(df$Difference)), color = "#000000", linewidth = 0.5, linetype = "dotted") +
+          ggplot2::geom_hline(yintercept = mean(df$Difference) + (1.96 * stats::sd(df$Difference)), color = "#000000", linewidth = 0.5, linetype = "dotted") +
+          ggplot2::coord_cartesian(ylim = input$rangeYBlandAltman) +
+          ggplot2::labs(title = "ActiGraph counts vs. agcounts", x = "ActiGraph vs. agcounts Averages", y = "Differences") +
+          ggplot2::theme_minimal() +
+          .agPlotTheme()
+      }
+      return(p)
+    }, res = 96)
+
+    # Reactable Data Table ----
+    output$comparisonReactableTable <- reactable::renderReactable({
+      shiny::req(comparisonData())
+      comparisonData <- comparisonData()
+      comparisonData <- comparisonData[comparisonData$Difference != 0, ]
+      reactable::reactable(
+        data = comparisonData,
+        columns = list(
+          time = reactable::colDef(name = "time", minWidth = 125),
+          agcounts = reactable::colDef(name = "agcounts", filterable = TRUE),
+          agd = reactable::colDef(name = "agd", filterable = TRUE),
+          Difference = reactable::colDef(name = "Difference", filterable = TRUE),
+          Average = reactable::colDef(name = "Average", filterable = TRUE)),
+        showPageSizeOptions = TRUE, pageSizeOptions = seq(5, 25, 5),
+        defaultPageSize = 5, searchable = TRUE, striped = TRUE,
+        highlight = TRUE, bordered = TRUE,
+        theme = .agReactableTheme())
+    })
+  })
+}
+
---
+++ R/countsModule.R
@@ -0,0 +1,113 @@
+#' @title countsModuleUI
+#' @description UI for countsModuleUI
+#' @noRd
+#' @keywords internal
+#' @importFrom reactable reactableOutput
+
+countsModuleUI <- function(id){
+  ns <- shiny::NS(id)
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(
+      shiny::sliderInput(ns("epoch"), "What epoch level?", min = 1, max = 60, value = 30, step = 1, post = " seconds", ticks = FALSE),
+      shiny::checkboxInput(ns("lfe"), "Add a low frequency extension filter?", value = FALSE),
+      shiny::HTML("<h5><b>Plot Settings for Calculate Counts</b></h5>"),
+      shiny::selectInput(ns("axisCounts"), "Counts Axis", choices = c("Axis1", "Axis2", "Axis3", "Vector.Magnitude"), selected = "Vector.Magnitude"),
+      shiny::checkboxInput(ns("excludeZeros"), "Exclude zeros from the plot?", value = FALSE),
+      shiny::numericInput(ns("binwidthCounts"), "Select a frequency polygon binwidth", value = 30, step = 10),
+      shiny::textInput(ns("countsPlotColor"), "Plot Color (accepts color name or hex code)", value = "#000000"),
+      shiny::sliderInput(ns("rangeCounts"), "Select a range for the X axis", value = c(0, 2000), min = 0, max = 10000, post = " counts")
+    ),
+    shiny::mainPanel(
+      shiny::tabsetPanel(
+        id = "countsTabset",
+        shiny::tabPanel("Visualization",
+                        shiny::plotOutput(ns("countsPlot"), width = "100%")),
+        shiny::tabPanel("Data",
+                        shiny::HTML("<h5>Total and average accelerometer counts from agcounts</h5>"),
+                        reactable::reactableOutput(ns("countsReactableTable")))
+      )
+    )
+  )
+}
+
+#' @title countsModuleServer
+#' @description Server for countsModuleServer
+#' @noRd
+#' @keywords internal
+#' @importFrom stringr regex
+#' @importFrom dplyr mutate group_by summarise
+#' @importFrom reactable renderReactable reactable colDef colGroup
+#' @importFrom grDevices colors
+
+countsModuleServer <- function(id, filteredData){
+  shiny::moduleServer(id, function(input, output, session){
+
+    # Data Processing ----
+    calculatedCounts <- shiny::reactive({
+      shiny::req(filteredData())
+      raw <- filteredData()[, 1:4]
+      class(raw) <- "data.frame"
+      calculate_counts(raw, input$epoch, lfe_select = input$lfe, tz = "UTC")
+    })
+
+    # Visualization ----
+    output$countsPlot <- shiny::renderPlot({
+      shiny::req(calculatedCounts())
+      .data <- NULL
+      countData <- calculatedCounts()
+      hexFormat <- stringr::regex("^#([A-Fa-f0-9]{6})$")
+      color <- input$countsPlotColor
+      defaultColor <- "#000000"
+      countsColor <- ifelse(color %in% grDevices::colors() | grepl(hexFormat, color), color, defaultColor)
+      if(input$excludeZeros) countData <- countData[countData[, input$axisCounts] > 0, ]
+      ggplot2::ggplot(data = countData, ggplot2::aes(x = .data[[input$axisCounts]])) +
+        ggplot2::geom_histogram(binwidth = input$binwidthCounts, fill = countsColor, color = "black") +
+        ggplot2::coord_cartesian(xlim = input$rangeCounts) +
+        ggplot2::labs(title = "Histogram of ActiGraph Counts", y = "Frequency") +
+        ggplot2::theme_minimal() +
+        .agPlotTheme()
+    }, res = 96)
+
+    # Reactable Data Table ----
+    output$countsReactableTable <- reactable::renderReactable({
+      shiny::req(calculatedCounts())
+      Date <- NULL
+      calculatedCounts() %>%
+        dplyr::mutate(Date = as.Date(time)) %>%
+        dplyr::group_by(Date) %>%
+        dplyr::summarise(sAxis1 = sum(Axis1), mAxis1 = round(mean(Axis1)),
+                         sAxis2 = sum(Axis2), mAxis2 = round(mean(Axis2)),
+                         sAxis3 = sum(Axis3), mAxis3 = round(mean(Axis3)),
+                         sVM = sum(Vector.Magnitude), mVM = round(mean(Vector.Magnitude))) %>%
+        reactable::reactable(columns = list(
+          Date = reactable::colDef(name = "Date", minWidth = 125, align = "center"),
+          sAxis1 = reactable::colDef(name = "Sum", align = "center"),
+          mAxis1 = reactable::colDef(name = "Mean", align = "center"),
+          sAxis2 = reactable::colDef(name = "Sum", align = "center"),
+          mAxis2 = reactable::colDef(name = "Mean", align = "center"),
+          sAxis3 = reactable::colDef(name = "Sum", align = "center"),
+          mAxis3 = reactable::colDef(name = "Mean", align = "center"),
+          sVM = reactable::colDef(name = "Sum", align = "center"),
+          mVM = reactable::colDef(name = "Mean", align = "center")
+        ),
+        columnGroups = list(
+          reactable::colGroup(name = "Axis 1", columns = c("sAxis1", "mAxis1")),
+          reactable::colGroup(name = "Axis 2", columns = c("sAxis2", "mAxis2")),
+          reactable::colGroup(name = "Axis 3", columns = c("sAxis3", "mAxis3")),
+          reactable::colGroup(name = "Vector Magnitude", columns = c("sVM", "mVM"))
+          ),
+          showPageSizeOptions = TRUE, pageSizeOptions = seq(5, 25, 5),
+          defaultPageSize = 5, searchable = TRUE, striped = TRUE,
+          highlight = TRUE, bordered = TRUE,
+          theme = .agReactableTheme())
+    })
+  })
+}
+
+
+
+
+
+
+
+
