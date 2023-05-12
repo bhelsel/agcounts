1289c31c907e2c07688bb48a47f41946f2454c03
Brian Helsel <bhelsel80@gmail.com>
2023-05-12 11:26:23 -0500

    Update test_gcalibrateC.R

--- tests/testthat/test_gcalibrateC.R
+++ tests/testthat/test_gcalibrateC.R
@@ -1,6 +1,10 @@
 
 testthat::test_that("Check to see if gcalibrateC is getting the same result as GGIR g.calibrate", {
 
+  # Skip on linux and solaris operating systems since it seems that decimal
+  # values are either rounded or stored differently on these opearting systems.
+  skip_on_os(os = c("linux", "solaris"))
+
   # Load Data
   load(system.file("extdata/calibrationXaxis.RData", package = "agcounts"))
   load(system.file("extdata/calibrationYaxis.RData", package = "agcounts"))
